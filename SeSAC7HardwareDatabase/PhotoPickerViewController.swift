c929203ef5dbfe719310765191eec9b2ea2c4fb1
hyeonjin6767 <parkhj067670@gmail.com>
2025-09-10 23:14:02 +0900

    UIImagePickerController / URLSession

---
+++ SeSAC7HardwareDatabase/PhotoPickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoPickerViewController.swift
+//  SeSAC7HardwareDatabase
+//
+//  Created by 박현진 on 9/10/25.
+//
+
+import UIKit
+
+class PhotoPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
